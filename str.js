747830c450f8ccfb41a4206aff9b2cf806292706
prtybyz <82148639+prtybyz@users.noreply.github.com>
2021-04-09 00:22:13 +0700

    Add files via upload

---
+++ str.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.animate', {
+       strings: [
+         'PRTYBYZ',
+         'Script Writter'
+         ],
+         typeSpeed: 100,
+         backSpeed: 100,
+         loop: true
+     });
