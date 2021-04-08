ffbe0f75d92e9bfcb89f3cd0bc7ec7c73b2c6a75
prtybyz <82148639+prtybyz@users.noreply.github.com>
2021-04-08 17:04:33 +0700

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,16 @@
+var typed = new Typed('#typed', {
+    stringsElement:'#typed-strings',
+    typeSpeed: 100,
+    loop: false,
+    backSpeed: 100,
+    smartBackspace: true,
+    cursorChar: "_"
+  });
+var typed1 = new Typed('#typed1', {
+    stringsElement:'#typed-strings1',
+    typeSpeed: 100,
+    loop: false,
+    backSpeed: 100,
+    smartBackspace: true,
+    cursorChar: "|"
+  });
